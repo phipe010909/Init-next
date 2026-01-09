f1aafac33b0ce6bbc8d21bab0c9459e38e86c518
phipe010909 <felipehoyospichina402@gmail.com>
2026-01-09 18:00:12 -0500

    Contruccion del Hero

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,23 @@
-import Boton from "@/components/Boton";
-
+import { CTA } from '../components/CTA';
+import Image from 'next/image';
 export default function Home() {
   return (
-    <div className="flex flex-col justify-center items-center h-screen">
-      <h1>Página principal</h1>
-
-      <Boton/>
+    <div className="flex flex-col items-center text-center min-h-screen justify-center mx-auto bg-[url('/hero-campestre.jpg')] bg-cover bg-center bg-[#000000]/60 bg-blend-overlay">
+      <div>
+        <Image
+          src="/logo-blanco.png"
+          alt="logo principal"
+          width={120}
+          height={120}
+          className=""
+          priority
+        />
+      </div>
+      <div className="flex flex-col items-center gap-6 text-white max-w-2xl">
+          <h1 className="text-5xl">Vive la tranquilidad que siempre soñaste</h1>
+          <p className="text-xl opacity-90">Lotes campestres en un entorno natural para vivir, invertir y disfrutar.</p>
+        <CTA />
+      </div>
     </div>
   );
 }
-
-
---
+++ components/CTA.tsx
@@ -0,0 +1,3 @@
+export const CTA = () => (
+  <button className="relative z-10 bg-gradient-to-r from-[#8F5A2F] to-[#A67B52] text-[#1E130C] px-10 py-3 rounded-full font-medium text-lg hover:brightness-110 transition-all shadow-lg">Agenda tu visita</button>
+);
